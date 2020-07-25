810c27e3a62a97cc808acdca00820d809427d5d0
Matias Barrera <matias.barrera95@gmail.com>
2020-07-25 17:55:26 -0400

    feat: mocks with faker.js

--- test/controllers/authController.test.js
+++ test/controllers/authController.test.js
@@ -1,4 +1,5 @@
 const httpMocks = require('node-mocks-http');
+const faker = require('faker');
 const authController = require('../../controllers/authController');
 const User = require('../../models/User');
 const databaseHandler = require('../helpers/databaseHandler');
@@ -6,6 +7,12 @@ const databaseHandler = require('../helpers/databaseHandler');
 let req;
 let res;
 let next;
+const mockedPassword = '123123';
+const {
+  internet: { userName, email }
+} = faker;
+const mockedUser = { name: userName(), email: email(), password: mockedPassword };
+const mockeduserSignin = { email: email(), password: mockedPassword };
 
 beforeEach(() => {
   req = httpMocks.createRequest();
@@ -18,7 +25,7 @@ describe('Login Controller', () => {
 
   afterAll(async () => databaseHandler.close());
 
-  beforeEach(async () => databaseHandler.clearAll());
+  afterEach(async () => databaseHandler.clearAll());
 
   // ======================================================
   // SingUp TEST
@@ -29,15 +36,15 @@ describe('Login Controller', () => {
     });
 
     describe('when user inputs invalid data', () => {
-      const mockedUser = { name: 'name', email: 'emailtestcl', password: 'password' };
-      const mockedUser2 = { name: 'Naruto', email: 'notemail', password: 'password ' };
-      it('returns 400 status if name length is minor than 6', () => {
-        req.body = mockedUser;
+      it('returns status 400 if name length is minor than 6', () => {
+        const mockedUser1 = { name: 'name', email: email(), password: mockedPassword };
+        req.body = mockedUser1;
         authController.signUp(req, res, next);
         expect(res.statusCode).toBe(400);
       });
 
       it('returns an error message', () => {
+        const mockedUser2 = { name: 'Naruto', email: 'it is not email', password: mockedPassword };
         req.body = mockedUser2;
         authController.signUp(req, res, next);
         // to-do: here this should import the validates errors... or not (?)
@@ -46,14 +53,11 @@ describe('Login Controller', () => {
     });
 
     describe('when user inputs valid data', () => {
-      const mockedUser = { name: 'administrador', email: 'admin@admin.cl', password: 'password' };
-
       beforeEach(async () => {
         req.body = mockedUser;
-        await User.deleteMany({});
       });
 
-      it('returns 201 status', async () => {
+      it('returns status 201', async () => {
         await authController.signUp(req, res, next);
         expect(res.statusCode).toBe(201);
       });
@@ -67,26 +71,41 @@ describe('Login Controller', () => {
   });
 
   // ======================================================
-  // SignIn TEST // TODO: standby
+  // SignIn TEST
   // ======================================================
   describe('signIn', () => {
+    beforeEach(async () => {
+      await User.deleteMany({});
+    });
+
     it('singIn is a function', () => {
       expect(typeof authController.signIn).toBe('function');
     });
 
     describe('when user inputs invalid data', () => {
-      const mockedUser = { email: 'emailtestcaaaa' };
-      const mockedUser2 = { email: 'test2test2cla', password: 'password' };
-      it('return 400 status if user email invalid format', () => {
-        req.body = mockedUser;
+      it('returns status 400 if user email invalid format', () => {
+        const mockedUser1 = { email: 'it is not email', password: mockedPassword };
+        req.body = mockedUser1;
         authController.signIn(req, res, next);
         expect(res.statusCode).toBe(400);
       });
 
-      it('return an error message', () => {
+      it('returns an error message', () => {
+        const mockedUser2 = { email: 'test2test2cla', password: mockedPassword };
         req.body = mockedUser2;
         authController.signIn(req, res, next);
         expect(res._getData()).toBe('"email" must be a valid email');
+      });
+    });
+
+    describe('when user inputs valid data', () => {
+      beforeEach(() => {
+        req.body = mockeduserSignin;
+      });
+
+      it('returns status 200', async () => {
+        authController.signIn(req, res, next);
+        expect(res.statusCode).toBe(200);
       });
     });
   });
